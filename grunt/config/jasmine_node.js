fea5e2cb5c7aba8cca46621786d8628641c2b639
Valentin Vago <zeropaper@irata.ch>
2014-06-18 08:47:57 +0200

    improve(test): allow a single jasmine-node test to be runned

--- grunt/config/jasmine_node.js
+++ grunt/config/jasmine_node.js
@@ -17,7 +17,7 @@ module.exports = function() {
     },
 
     unit: [
-      process.env.JASMINE_TEST_ONLY ? 'test/jasmine_node/'+ process.env.JASMINE_TEST_ONLY +'Spec.js' : 'test/jasmine_node/**/*Spec.js'
+      process.env.JASMINE_TEST_ONLY ? ('test/jasmine_node/'+ process.env.JASMINE_TEST_ONLY +'Spec.js') : 'test/jasmine_node/**/*Spec.js'
     ]
   };
 };
